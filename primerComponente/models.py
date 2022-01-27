18415c73e87046d4420f5a60ccaadbe48fb9dee5
203445 <203445@ids.upchiapas.edu.mx>
2022-01-27 17:02:00 -0600

    gitignore problems uploaded everything again

---
+++ primerComponente/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+#importación de forma directa
+import datetime
+
+
+class PrimerModelo(models.Model):
+    campo_uno = models.CharField(max_length=255, null=False)
+    edad = models.IntegerField(null=False)
+    created = models.DateTimeField(default=timezone.now)
+    edit = models.DateTimeField(blank=True, null=True, default=None)
+
+class SegundpModelo(models.Model):
+    campo_uno = models.CharField(max_length=255, null=True)
+    edad = models.IntegerField(null=True, default=0)
+    created = models.DateTimeField(default=timezone.now)
+    edit = models.DateTimeField(blank=True, null=True, default=None)
+
+    class Meta:
+        db_table = 'segundo_modelo'
---
+++ primerComponente/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+# Importando vistas
+from primerComponente.views import PrimerViewList
+from primerComponente.views import PrimerViewDetail
+
+urlpatterns = [
+    re_path(r'^lista/$', PrimerViewList.as_view()), 
+    re_path(r'^lista/(?P<pk>\d+)$', PrimerViewDetail.as_view()),   
+]
