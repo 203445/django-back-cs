7b2fe4e7c63e295104ac3142b2e5cf1ea64d7730
203445 <203445@ids.upchiapas.edu.mx>
2022-01-27 23:01:49 -0600

    modified primerComponente/views add delete

--- primerComponente/views.py
+++ primerComponente/views.py
@@ -50,3 +50,13 @@ class PrimerViewDetail(APIView):
                 return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST) 
         else:
             return Response('ID no encontrado', status = status.HTTP_400_BAD_REQUEST) 
+
+    def delete(self, request, pk, format=None):
+        idResponse = self.get_object(pk = pk)
+       
+        if idResponse != 404:
+            idResponse.delete()
+            return Response({'message': 'usuario eliminado correctamente'})
+        else:
+            return Response('ID no encontrado', status =  status.HTTP_404_NOT_FOUND) 
+      
